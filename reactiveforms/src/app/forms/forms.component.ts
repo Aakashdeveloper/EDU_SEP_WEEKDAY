756af52496d32de391e4ac4e0a3af8e29aabc4ef
Avyaan <avi@Avyaans-MacBook-Pro.local>
2018-10-02 21:49:26 +0530

    a

--- reactiveforms/src/app/forms/forms.component.ts
+++ reactiveforms/src/app/forms/forms.component.ts
@@ -1,9 +1,29 @@
 import { Component, OnInit} from '@angular/core';
 import {FormGroup, FormControl, FormBuilder,
-         Validators} from '@angular/forms';
+         Validators, AbstractControl, ValidatorFn} from '@angular/forms';
 
 import { Customer } from './forms';
 
+function ratingrange(min: number, max: number): ValidatorFn {
+    return (c: AbstractControl): {[key: string]: boolean} |null => {
+        if (c.value !== undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
+            return{ 'range': true};
+        }
+        return null;
+    };
+}
+
+function emailMatcher(c: AbstractControl) {
+    const emailControl = c.get('email');
+    const confirmControl = c.get('Confirmemail');
+    if (emailControl.pristine || confirmControl.pristine ) {
+        return null;
+    }
+    if (emailControl.value === confirmControl.value) {
+        return null;
+    }
+    return{'match': true};
+}
 @Component({
     selector: 'app-forms',
     templateUrl: './forms.component.html'
@@ -17,7 +37,18 @@ export class FormsComponent implements OnInit  {
     ngOnInit(): void {
         this.customerForm = this.fb.group({
             firstName: ['John', [Validators.required, Validators.minLength(5)]],
-            lastName: ['Methew']
+            lastName: ['Methew', []],
+            emailGroup: this.fb.group({
+                email: ['a@a', [Validators.required, Validators.pattern('[a-z0-9]+@[a-z0-9]+')]],
+                Confirmemail: ['', [Validators.required]],
+            }, {validator: emailMatcher }),
+            rating: ['', ratingrange(1, 9)]
+        });
+    }
+
+    popuplateData(): void {
+        this.customerForm.patchValue({
+            email: 'j@a'
         });
     }
 
@@ -25,6 +56,7 @@ export class FormsComponent implements OnInit  {
         console.log(this.customerForm);
         console.log('Saved: ' + JSON.stringify(this.customerForm.value));
     }
+
  }
 
 
@@ -34,4 +66,13 @@ export class FormsComponent implements OnInit  {
             firstName: new FormControl(),
             lastName: new FormControl()
         });
+
+      popuplateData(): void {
+        this.customerForm.setValue({
+            firstName: 'Ankit',
+            lastName: 'Sharma',
+            email: 'j@a'
+        });
+    }
+
   */
